62cb270f7808ad8e20f4bf2bc0fe143aabdbb51a
“afloresbgeneral” <aflores@bgeneral.com>
2019-09-30 12:05:14 -0500

    prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,12 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: '<your-key>',
-    authDomain: '<your-project-authdomain>',
-    databaseURL: '<your-database-URL>',
-    projectId: '<your-project-id>',
-    storageBucket: '<your-storage-bucket>',
-    messagingSenderId: '<your-messaging-sender-id>'
+    apiKey: 'AIzaSyAH4E1v4Vv-MR3FK_8PbZeb-nsPyMBYUj4',
+    authDomain: 'firechat-477ec.firebaseapp.com',
+    databaseURL: 'https://firechat-477ec.firebaseio.com',
+    projectId: 'firechat-477ec',
+    storageBucket: '',
+    messagingSenderId: '174190766545',
+    appId: '1:174190766545:web:ae5a74de26793d452718f0'
   }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebase: {
     apiKey: 'AIzaSyAH4E1v4Vv-MR3FK_8PbZeb-nsPyMBYUj4',
     authDomain: 'firechat-477ec.firebaseapp.com',
